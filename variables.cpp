969991912e91daa64be990d6393daa7410c16ed8
Meddy <kaluumamhd@gmail.com>
2023-01-10 00:29:44 +0300

    init push with hello world and variables

---
+++ variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main () {
+    int var, age;
+    var = 5;
+    age = 20;
+
+    std::cout << var << std::endl;
+
+    // string concatenation
+    std::cout << "John Deo is " << age << " years old." << std::endl;
+    return 0;
+}
